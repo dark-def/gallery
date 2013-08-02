d10b2bac300adfa4dbb92f3201cd6efc58654f75
dark-def <drkmen@rambler.ru>
2013-08-02 11:27:39 +0300

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gallery::Application.config.secret_token = 'bf8fe889ea13012e0366585557ea33a84ecb04de315f4366ac36f8cf39994027dcb77de50ce4ddaacd9e1bfefe4019214b978c0c9d91aa2c6d72ea851df5d7be'
