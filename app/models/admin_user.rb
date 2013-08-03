8fcc8627f361d5636622d446359c53ade2e911bf
dark-def <drkmen@rambler.ru>
2013-08-03 19:15:12 +0300

    enable devise, active admin

--- app/models/admin_user.rb
+++ app/models/admin_user.rb
@@ -1,8 +1,8 @@
-class User < ActiveRecord::Base
+class AdminUser < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :token_authenticatable, :confirmable,
   # :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
+  devise :database_authenticatable, 
          :recoverable, :rememberable, :trackable, :validatable
-  attr_accessible :name, :email, :password, :password_confirmation
+  attr_accessible :title, :body, :email, :password, :password_confirmation
 end
