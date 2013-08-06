4d90321d6fb10abdc54129a35dabbfe1321958ce
dark-def <drkmen@rambler.ru>
2013-08-06 20:06:51 +0300

    working likes but number stil not updating

--- app/assets/javascripts/like.js
+++ app/assets/javascripts/like.js
@@ -1,19 +1,21 @@
 $(document).ready(function(){
-
-    $('.icon-thumbs-down-alt.icon-2x').hide();
-
+    var like_count
     $(document).ajaxSuccess(function(event, response, settings)  {
 
+        like_count = response.responseJSON.like_count
+
         if (response.responseJSON.status == 'success_added'){
-            console.log('YOU LIKE IT');
-            $('.icon-thumbs-up-alt.icon-2x').hide();
-            $('.icon-thumbs-down-alt.icon-2x').show();
+            like_count += 1
+            console.log(like_count);
+            $('.like_counter').html(like_count);
+            $('.icon-thumbs-up-alt.icon-2x').replaceWith('<a data-remote="true" href="/images/2/dislike"><div  class ="icon-thumbs-down-alt icon-2x"></div></a>');
         }
 
         if (response.responseJSON.status == 'success_deleted') {
-            console.log('YOU DONT LIKE IT');
-            $('.icon-thumbs-up-alt.icon-2x').show();
-            $('.icon-thumbs-down-alt.icon-2x').hide();
+            like_count -= 1
+            console.log(like_count);
+            $('.like_counter').html(like_count);
+            $('.icon-thumbs-down-alt.icon-2x').replaceWith('<a data-remote="true" href="/images/2/like"><div class="icon-thumbs-up-alt icon-2x"></div></a> ');
         }
     });
 
--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -3,25 +3,26 @@ class LikesController < ApplicationController
   before_filter :authenticate_user!, :only => [:create]
 
   def create
+    like_count = Image.find(params[:id]).likes.count
     @like = current_user.likes.build
     @like.user_id = current_user.id
-
     @like.image_id = params[:id]
     if @like.save
-      render :json => {:status => 'success_added' }
+      render :json => {:status => 'success_added', :like_count => like_count }
     end
   end
 
   def destroy
+    like_count = Image.find(params[:id]).likes.count
     like = current_user.likes.where("image_id = #{params[:id]}")
-    logger.info '='*100
-    logger.info like.inspect
-    logger.info params.inspect
-    logger.info '='*100
     if !like.blank?
       like.delete_all
-      render :json => {:status => 'success_deleted'}
+      render :json => {:status => 'success_deleted', :like_count => like_count}
+    else
+      render :json => {}
     end
+
   end
+
 
 end
