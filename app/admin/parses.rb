84b6d673ae6f09783be5fae4cf490f63de098a0f
dark-def <drkmen@rambler.ru>
2013-08-07 19:42:16 +0300

    end day

--- app/admin/parses.rb
+++ app/admin/parses.rb
@@ -4,25 +4,22 @@ ActiveAdmin.register_page "Parse" do
   require 'open-uri'
 
   page_action :steal, :method => :post do
+
     @images = Array.new
     site = params[:parse][:url]
 
     doc = Nokogiri::HTML(open(site))
-    #url = "http://www.walmart.com/search/search-ng.do?search_constraint=0&ic=48_0&search_query=batman&Find.x=0&Find.y=0&Find=Find"
-    #doc = Nokogiri::HTML(open(url))
 
     doc.css("img").each_with_index do |item, index|
       @images[index] = item['src']
     end
 
-    #puts doc.at_css("title").text
-    #doc.css(".item").each do |item|
-    #  title = item.at_css(".prodLink").text
-    #  price = item.at_css(".PriceCompare .BodyS, .PriceXLBold")
-    #  puts "#{title} - #{price}"
-    #  puts item.at_css(".prodLink")[:href]
-    #end
     render :layout => 'active_admin'
+  end
+
+  page_action :save_img, :method => :post do
+
+    render :json => {}, :layout => false
   end
 
 
