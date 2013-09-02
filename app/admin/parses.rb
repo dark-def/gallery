2482d0aea3473e25eeadcc94147ec878766d7fb4
dark-def <drkmen@rambler.ru>
2013-09-02 10:25:53 +0300

    removed locale to cookies

--- app/admin/parses.rb
+++ app/admin/parses.rb
@@ -12,7 +12,7 @@ ActiveAdmin.register_page "Parse" do
     doc.css("img").each_with_index do |item, index|
       @images[index] = item['src']
     end
-    #render :layout => 'active_admin' # сука конченная ересь, орет на симпл капчу
+    #render :layout => 'active_admin'
     render :layout => false
   end
 
