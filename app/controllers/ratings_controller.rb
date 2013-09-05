e3307e7bb5e3a06bf7c746dd90b482ca6b92c637
dark-def <drkmen@rambler.ru>
2013-09-05 19:41:17 +0300

    improves

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -2,6 +2,12 @@ class RatingsController < ApplicationController
 
   def create
 
+    image = Image.where(:id => params[:id]).first
+    rate = Rating.create(:user_id => current_user.id, :image_id => image.id, :mark => params[:mark])
+    if rate.save
+      ActiveSupport::Notifications.instrument('likes.create', :user_id => current_user.id, :url => request.fullpath)
+      render :json => {:status => 'success_rated', :image_id => image.id }
+    end
   end
 
 end
