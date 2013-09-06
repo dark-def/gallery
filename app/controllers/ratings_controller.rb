b24ec90ebe85c3ed41bb34a97c37b00555e407a9
dark-def <drkmen@rambler.ru>
2013-09-06 17:27:22 +0300

    use partials

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -2,14 +2,16 @@ class RatingsController < ApplicationController
 
   def create
 
-    image = Image.where(:id => params[:id]).first
-    rate = Rating.create(:user_id => current_user.id, :image_id => image.id, :mark => params[:mark])
+    @image = Image.where(:id => params[:id]).first
+    rate = Rating.create(:user_id => current_user.id, :image_id => @image.id, :mark => params[:mark])
     if rate.save
       ActiveSupport::Notifications.instrument("rate - #{params[:mark]}", :user_id => current_user.id, :url => request.fullpath)
-      rating = image.ratings.average('mark').round 1
-      Image.update(image.id, :rating => rating )
+      rating = @image.ratings.average('mark').round 1    # get average value with 1 symbol after dot
+      Image.update(@image.id, :rating => rating )       # update image rating field
 
-      render :json => {:status => 'success_rated', :image_id => image.id, :rating => rating }
+      respond_to do |format|
+        format.js {@image}
+      end
     end
   end
 
