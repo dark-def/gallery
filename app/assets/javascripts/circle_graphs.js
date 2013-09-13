9cdaedfb2b2870204c03657add80838010b47eb3
dark-def <drkmen@rambler.ru>
2013-09-13 19:34:32 +0300

    working circle graphs

--- app/assets/javascripts/circle_graphs.js
+++ app/assets/javascripts/circle_graphs.js
@@ -2,6 +2,57 @@ $(document).ready(function(){
 
     var img_size, comments_size, likes_size;
 
+//    var d = [
+//                {
+//                    "label": "One",
+//                    "value" : 29.765957771107
+//                } ,
+//                {
+//                    "label": "Two",
+//                    "value" : 0
+//                } ,
+//                {
+//                    "label": "Three",
+//                    "value" : 32.807804682612
+//                } ,
+//                {
+//                    "label": "Four",
+//                    "value" : 196.45946739256
+//                } ,
+//                {
+//                    "label": "Five",
+//                    "value" : 0.19434030906893
+//                } ,
+//                {
+//                    "label": "Six",
+//                    "value" : 98.079782601442
+//                } ,
+//                {
+//                    "label": "Seven",
+//                    "value" : 13.925743130903
+//                } ,
+//                {
+//                    "label": "Eight",
+//                    "value" : 5.1387322875705
+//                }
+//            ];
+//
+//    nv.addGraph(function() {
+//        var chart = nv.models.pieChart()
+//            .x(function(d) {  return d.label })
+//            .y(function(d) {  return d.value })
+//            .showLabels(true);
+//
+//        d3.select("#chart1 svg")
+//            .datum(d)
+//            .transition().duration(1200)
+//            .call(chart);
+//
+//        return chart;
+//    });
+
+
+
     $.ajax({
         url: "/events/get_circle_graphs",
         type: "post",
@@ -12,9 +63,7 @@ $(document).ready(function(){
             comments_size = response.comments_size;
             likes_size = response.likes_size;
             var data = [
-                {
-                    key: "Cumulative Return",
-                    values: [
+
                         {
                             "label": "One",
                             "value" : 29.765957771107
@@ -31,8 +80,7 @@ $(document).ready(function(){
                             "label": "Four",
                             "value" : 196.45946739256
                         }
-                    ]
-                }
+
             ];
             console.log(data)
 
