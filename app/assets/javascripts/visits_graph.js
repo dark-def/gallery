b672e1cda2dd2519222af28aee7a1f1db620e526
dark-def <drkmen@rambler.ru>
2013-09-13 18:25:27 +0300

    removed commited code

--- app/assets/javascripts/visits_graph.js
+++ app/assets/javascripts/visits_graph.js
@@ -47,9 +47,6 @@ $(document).ready(function(){
                         return d3.time.format('%x')(new Date(d))
                     });
 
-//                chart.yAxis
-//                    .tickFormat(d3.format(',.1%'));
-
                 d3.select('#chart_sign svg')
                     .datum(data)
                     .transition().duration(500)
@@ -59,54 +56,6 @@ $(document).ready(function(){
 
                 return chart;
             });
-
-
-
-
-
-
-
-
-
-
-
-
-//            function stream_index(d, i) {
-//                return {x: i, y: Math.max(0, d)};
-//            }
-
-//            /* Inspired by Lee Byron's test data generator. */
-//            function stream_layers(n, m, o) {
-//                if (arguments.length < 3) o = 0;
-//                function bump(a) {
-//                    var x = 1 / (.1 + Math.random()),
-//                        y = 2 * Math.random() - .5,
-//                        z = 10 / (.1 + Math.random());
-//                    for (var i = 0; i < m; i++) {
-//                        var w = (i / m - y) * z;
-//                        a[i] += x * Math.exp(-w * w);
-//                    }
-//                }
-//                return d3.range(n).map(function() {
-//                    var a = [], i;
-//                    for (i = 0; i < m; i++) a[i] = o + o * Math.random();
-//                    for (i = 0; i < 5; i++) bump(a);
-//                    return a.map(stream_index);
-//                });
-//            }
-//
-//            function stream_index(d, i) {
-//                return {x: i, y: Math.max(0, d)};
-//            }
-
-
-
-
-
-
-
-
-
 
         }
     });
